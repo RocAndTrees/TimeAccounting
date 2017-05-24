8e5b9bdacf0312d8f10248c1c099973140e51d46
linxiaopeng <359539865@qq.com>
2017-05-24 11:07:36 +0800

    init TimeAccountingProject

---
+++ TimeAccountingProject/TimeAccountingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeAccountingProject
+//
+//  Created by pxl on 2017/5/24.
+//  Copyright © 2017年 pxl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
+
