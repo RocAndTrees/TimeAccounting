39448e42830d1133fbffc33cc49deba250c18a66
linxiaopeng <359539865@qq.com>
2017-05-24 16:34:28 +0800

    no message

---
+++ TimeAccountingMacOS/TimeAccountingMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TimeAccountingMacOS
+//
+//  Created by pxl on 2017/5/24.
+//  Copyright © 2017年 pxl. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ TimeAccountingProject/TimeAccountingProject/ChartViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChartViewController.swift
+//  TimeAccountingProject
+//
+//  Created by pxl on 2017/5/24.
+//  Copyright © 2017年 pxl. All rights reserved.
+//
+
+import UIKit
+
+import Charts
+
+class ChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
